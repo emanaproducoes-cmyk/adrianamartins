50971ff1ad94bd90ab845b3f585c2bdec4e3acd3
emanaproducoes-cmyk <emanaproducoes@gmail.com>
2026-08-15 11:39:02 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,7 +35,7 @@ export function Navbar() {
             width={160}
             height={44}
             priority
-            className="block h-9 w-auto object-contain transition-transform duration-300 group-hover/logo:scale-105 sm:h-10 md:h-11"
+            className="block h-10 w-auto object-contain transition-transform duration-300 group-hover/logo:scale-105"
           />
 
           {/* Efeito scanner na marca */}
